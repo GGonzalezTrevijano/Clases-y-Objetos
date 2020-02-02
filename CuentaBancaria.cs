01362e78222b328bdbd006eada8a670aeef25c75
Guillermo <gonzaleztrevijano@gmail.com>
2020-02-02 13:17:51 +0100

    Clases y Objetos

---
+++ CuentaBancaria.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto2_Clases_y_Objetos
+{
+    class CuentaBancaria
+    {
+        //Propiedades automáticas
+        public string NoCuenta { get; set; }
+        public string Usuario { get; set; }
+        
+        //Backing field (necesario para una propiedad completa)
+        private decimal _saldo;
+
+        //Propiedad Completa
+        public decimal Saldo
+        {
+            get { return _saldo; }
+            set
+            {
+                //Evitar que el saldo sea negativo
+                if (value < 0)
+                {
+                    _saldo = 0;
+                }
+                else
+                {
+                    _saldo = value;
+                }
+            }
+        }
+
+        //Propiedad estática
+        public static string Banco { get; set; }    //static sirve para convertir la propiedad en estatica
+
+
+        #region constructores                   //region sirve para crear a la izquierda un campo en el que minimizar el código
+        public CuentaBancaria(string noCuenta)
+        {
+            NoCuenta = noCuenta;        
+        }
+
+        public CuentaBancaria(string noCuenta, string usuario) : this(noCuenta)
+        {
+            Usuario = usuario;
+        }
+
+        public CuentaBancaria(string noCuenta, string usuario, decimal saldo) : this(noCuenta, usuario)
+        {
+            Saldo = saldo;        
+        }
+        //Constructor Vacío (default)
+        public CuentaBancaria() { }
+        #endregion
+
+        #region Métodos de instancia
+        //Métodos de instancia
+
+        public void Retirar(decimal cantidad)
+        {
+            if (cantidad > Saldo)
+            {
+                Console.WriteLine("Saldo insuficiente");
+                return;  // No continua leyendo el código si no se cumple la condicion
+            }
+
+            Saldo -= cantidad; //Esto quiere decir Saldo = Saldo - Cantidad.
+        }
+
+        public void Depostiar(decimal cantidad)
+        {
+            Saldo += cantidad;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("NoCuenta: {0}, Usuario: {1}, Saldo: {2}$", NoCuenta, Usuario, Saldo);  //Con está llamada generas un texto que luego se va a mostrar en el programa principal
+        }
+        #endregion
+
+        public static void AsignarBanco(string banco)
+        {
+            Console.WriteLine("Asignando nuevo banco...");
+            Banco = banco;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Proyecto2_Clases_y_Objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OBJETOS
+
+            //Crear un objeto de tipo rectangulo
+
+            Console.WriteLine("Creación de Objetos");
+                
+            Console.Write("\r\n");
+
+            Rectangulo rec = new Rectangulo();
+            rec.Base = 10;
+            rec.Altura = 5;
+
+            int area = rec.CalcularArea();
+
+            Console.WriteLine("El area del Rectangulo con Base {0} y Altura {1} es = {2}", rec.Base, rec.Altura, area);          
+
+            //Otra forma de crear un objeto
+            Rectangulo rec2 = new Rectangulo { Base = 3, Altura = 8 };
+
+            int area2 = rec2.CalcularArea();
+               
+            Console.WriteLine("El area del Rectangulo con Base {0} y Altura {1} es = {2}", rec2.Base, rec2.Altura, area2);
+
+            Console.Write("\r\n");
+
+            //CONSTRUCTORES
+
+            Console.WriteLine("Creación de Constructores");
+
+            Console.Write("\r\n");
+
+            CuentaBancaria cuenta1 = new CuentaBancaria("01", "Juan Guaido", 5000);
+            Console.WriteLine("NoCuenta: {0}, Usuario: {1}, Saldo: {2}", cuenta1.NoCuenta,
+                cuenta1.Usuario, cuenta1.Saldo);
+
+            CuentaBancaria cuenta2 = new CuentaBancaria("02", "Guillermo Gonzalez", 10000);
+            Console.WriteLine("NoCuenta: {0}, Usuario: {1}, Saldo: {2}", cuenta2.NoCuenta,
+                cuenta2.Usuario, cuenta2.Saldo);
+
+            Console.Write("\r\n");
+
+            Console.WriteLine("Prueba de Propiedades");
+
+            Console.Write("\r\n");
+
+            CuentaBancaria cuenta3 = new CuentaBancaria();
+
+            cuenta3.Saldo = -5; 
+            Console.WriteLine("El Saldo es: {0}", cuenta3.Saldo);
+
+            cuenta3.Saldo = 10;
+            Console.WriteLine("El Saldo tras el ingreso es: {0}", cuenta3.Saldo);
+
+            Console.Write("\r\n");
+
+            //Metodos de Instancia
+
+            Console.WriteLine("Prueba de Metodos de instancia");
+
+            Console.Write("\r\n");
+
+            CuentaBancaria cuenta4 = new CuentaBancaria("03", "Santiago Abascal", 5000);
+            CuentaBancaria cuenta5 = new CuentaBancaria("04", "Pablo Iglesias", 1000);
+
+            Console.WriteLine("Depostiar $50 euros en cuenta4");
+            cuenta4.Depostiar(50);  //Ingresa 50 euros en la cuenta4
+
+            Console.WriteLine("Retirar $20 euros en cuenta5");
+            cuenta5.Retirar(20);    //Retira 20 euros en la cuenta5
+
+            Console.Write("\r\n");
+
+            Console.WriteLine(cuenta4.ToString());  //La llamada ToString imprime la cadena creada en la clase con los valores asigados a esta.
+            Console.WriteLine(cuenta5.ToString());
+
+            Console.Write("\r\n");
+
+            //Miembros estáticos
+            //Para acceder a los miembros estáticos se usa solo el nombre de la clase
+
+            Console.WriteLine("Prueba de Métodos estaticos");
+
+            Console.Write("\r\n");
+
+            CuentaBancaria.Banco = "Mi banco";
+
+            Console.WriteLine(cuenta4);
+            Console.WriteLine(cuenta5);
+
+            Console.Write("\r\n");
+
+            Console.WriteLine("Cambiando el banco...");
+
+            Console.Write("\r\n");
+
+            CuentaBancaria.AsignarBanco("Otro banco");
+
+            Console.Write("\r\n");
+
+            Console.WriteLine(cuenta4);
+            Console.WriteLine(cuenta5);
+        }
+    }
+}
